011cfe7fbe2f38ce04731b15180103a2e94ff13c
AirJonas <77889765+AirJonas@users.noreply.github.com>
2024-12-04 14:40:46 +0100

    fixed some problems

--- index.js
+++ index.js
@@ -5,16 +5,32 @@ let snakePic = document.getElementById('snakeImg')
 
 
 const maxPixelsConst = 145
-let pixelsHung = maxPixelsConst
-let pixelsHappy = maxPixelsConst
-let pixelsClean = maxPixelsConst
-happynes.style.backgroundColor = 'green';
-hunger.style.backgroundColor = 'green';
-cleanliness.style.backgroundColor = 'green';
+let pixelsHung 
+let pixelsHappy 
+let pixelsClean 
+let lower
 
-    // setup()
+    start()
+function start(){
+    document.getElementById('happynessbtn').classList.remove('hidden')
+    document.getElementById('healthbtn').classList.remove('hidden')
+    document.getElementById('hungerbtn').classList.remove('hidden')
+    document.getElementById('PlayAgain').classList.add('hidden')
+     pixelsHung = maxPixelsConst
+     pixelsHappy = maxPixelsConst
+     pixelsClean = maxPixelsConst
+     hunger.style.backgroundColor = 'green';
+     happynes.style.backgroundColor = 'green';
+     cleanliness.style.backgroundColor = 'green';
+     hunger.style.width = "145px";
+     happynes.style.width = "145px";
+     cleanliness.style.width = "145px";
+     changePic('')
 
-let lower = setInterval(lowerTheBars, 500)
+
+
+    lower = setInterval(lowerTheBars, 10)
+}
 
 function fillHunger(){
     hunger.style.backgroundColor = 'green';
@@ -42,20 +58,20 @@ function fillCleanless(){
 
 
 function lowerTheBars(){
-    pixelsHappy -= 15
-    pixelsHung -= 10
-    pixelsClean -= 12
+    pixelsHappy -= 2
+    pixelsHung -= 3
+    pixelsClean -= 1
  
     
     happynes.style.width = "" + pixelsHappy + "px";
     hunger.style.width = "" + pixelsHung + "px";
     cleanliness.style.width = "" + pixelsClean + "px";
 
-    if(pixelsHappy < 80 && pixelsHappy > 0){
+    if(pixelsHappy < 50 && pixelsHappy > 0){
         happynes.style.backgroundColor = 'red';
         changePic('sad')
     } 
-    else if(pixelsHappy >= 80){
+    else if(pixelsHappy >= 50){
         happynes.style.backgroundColor = 'green';
     } 
     else {
@@ -64,11 +80,11 @@ function lowerTheBars(){
         changePic('death')
     }
 
-    if (pixelsHung < 80 && pixelsHung > 0){
+    if (pixelsHung < 50 && pixelsHung > 0){
         hunger.style.backgroundColor = 'red';
         changePic('hungry');
     }
-    else if(pixelsHung >= 80){
+    else if(pixelsHung >= 50){
         hunger.style.backgroundColor  = 'green'
     }
     else{
@@ -76,11 +92,11 @@ function lowerTheBars(){
         gameOver()
     }
 
-    if(pixelsClean < 80 && pixelsClean > 0){
+    if(pixelsClean < 50 && pixelsClean > 0){
         cleanliness.style.backgroundColor = 'red'
         changePic('dirty')
     } 
-    else if(pixelsClean >= 80){
+    else if(pixelsClean >= 50){
         cleanliness.style.backgroundColor = 'green';
     } 
     else {
@@ -92,6 +108,12 @@ function lowerTheBars(){
 function gameOver(){
   clearInterval(lower)
   changePic('death')
+
+  document.getElementById('happynessbtn').classList.add('hidden')
+  document.getElementById('healthbtn').classList.add('hidden')
+  document.getElementById('hungerbtn').classList.add('hidden')
+  document.getElementById('PlayAgain').classList.remove('hidden')
+  
 }
 
 function changePic(pic){
